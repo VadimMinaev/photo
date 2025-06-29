6c9eac12a25b0eabeca021a70e89167adc161dea
Vadmin <vadmin@rr-th.com>
2025-06-29 18:02:52 +0300

    fix

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# import os
+from flask import Flask, render_template, request, redirect, url_for
+import os
 
-# app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = 'static/uploads'
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'static/uploads'
 
-# @app.route('/')
-# def gallery():
-#     images = os.listdir(app.config['UPLOAD_FOLDER'])
-#     return render_template('index.html', images=images)
+@app.route('/')
+def gallery():
+    images = os.listdir(app.config['UPLOAD_FOLDER'])
+    return render_template('index.html', images=images)
 
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'photo' in request.files:
-#         file = request.files['photo']
-#         if file.filename != '':
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#     return redirect(url_for('gallery'))
+@app.route('/upload', methods=['POST'])
+def upload():
+    if 'photo' in request.files:
+        file = request.files['photo']
+        if file.filename != '':
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return redirect(url_for('gallery'))
 
 import os
 from flask import Flask, render_template, request, redirect, url_for
