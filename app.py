07c4e577014020cafd2e5cfe63037cb42bfc029e
Vadmin <vadmin@rr-th.com>
2025-06-29 17:57:54 +0300

    обнова

--- app.py
+++ app.py
@@ -40,17 +40,39 @@ def album(album_name):
     photos = [photo for photo in photos if photo.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
     return render_template('album.html', album=album_name, photos=photos)
 
+# @app.route('/upload', methods=['GET', 'POST'])
+# def upload():
+#     albums = [name for name in os.listdir(UPLOAD_FOLDER)
+#               if os.path.isdir(os.path.join(UPLOAD_FOLDER, name))]
+#     if request.method == 'POST':
+#         album = request.form.get('album')
+#         file = request.files.get('photo')
+#         if album and file and file.filename != '':
+#             album_path = os.path.join(UPLOAD_FOLDER, album)
+#             if not os.path.exists(album_path):
+#                 os.makedirs(album_path)
+#             file.save(os.path.join(album_path, file.filename))
+#             return redirect(url_for('album', album_name=album))
+#     return render_template('upload.html', albums=albums)
+
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     albums = [name for name in os.listdir(UPLOAD_FOLDER)
               if os.path.isdir(os.path.join(UPLOAD_FOLDER, name))]
     if request.method == 'POST':
+        # Получаем имя выбранного альбома и нового альбома
         album = request.form.get('album')
+        new_album = request.form.get('new_album').strip()
+
+        # Если введено имя нового альбома, используем его
+        if new_album:
+            album = new_album
+
         file = request.files.get('photo')
         if album and file and file.filename != '':
             album_path = os.path.join(UPLOAD_FOLDER, album)
             if not os.path.exists(album_path):
-                os.makedirs(album_path)
+                os.makedirs(album_path)  # Создаем новую папку-альбом
             file.save(os.path.join(album_path, file.filename))
             return redirect(url_for('album', album_name=album))
     return render_template('upload.html', albums=albums)
