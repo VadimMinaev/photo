c6a2cf4f200f32bf4abdbf74e8f51a9f7139735b
Vadmin <vadmin@rr-th.com>
2025-06-29 19:01:24 +0300

    Update

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 import os
+import json
 from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 WALLPAPER_FOLDER = 'static/images'
+SETTINGS_FILE = 'user_settings.json'
+
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['WALLPAPER_FOLDER'] = WALLPAPER_FOLDER
 
@@ -35,41 +38,3 @@ def upload():
         file = request.files.get('photo')
         if album and file and file.filename != '':
             album_path = os.path.join(UPLOAD_FOLDER, album)
-            if not os.path.exists(album_path):
-                os.makedirs(album_path)
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(album_path, filename))
-            return redirect(url_for('album', album_name=album))
-    return render_template('upload.html', albums=albums)
-
-@app.route('/settings', methods=['GET', 'POST'])
-def settings():
-    message = ''
-    if request.method == 'POST':
-        file = request.files.get('wallpaper')
-        if file and file.filename != '':
-            filename = secure_filename(file.filename)
-            wallpaper_path = os.path.join(WALLPAPER_FOLDER, filename)
-            file.save(wallpaper_path)
-            message = 'Обои успешно загружены!'
-            with open('current_wallpaper.txt', 'w') as f:
-                f.write(filename)
-        else:
-            message = 'Ошибка загрузки файла.'
-    wallpaper_filename = None
-    if os.path.exists('current_wallpaper.txt'):
-        with open('current_wallpaper.txt', 'r') as f:
-            wallpaper_filename = f.read().strip()
-    return render_template('settings.html', message=message, wallpaper=wallpaper_filename)
-
-@app.context_processor
-def inject_wallpaper():
-    wallpaper_url = None
-    if os.path.exists('current_wallpaper.txt'):
-        with open('current_wallpaper.txt', 'r') as f:
-            filename = f.read().strip()
-            wallpaper_url = url_for('static', filename='images/' + filename)
-    return dict(wallpaper_url=wallpaper_url)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
