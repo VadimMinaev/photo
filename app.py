55876d49de642f54a33dfa190af747aa47fd14ee
Vadmin <vadmin@rr-th.com>
2025-06-29 18:08:04 +0300

    fix

--- app.py
+++ app.py
@@ -1,68 +1,18 @@
-# from flask import Flask, render_template, request, redirect, url_for
-# import os
-
-# app = Flask(__name__)
-# app.config['UPLOAD_FOLDER'] = 'static/uploads'
-
-# @app.route('/')
-# def gallery():
-#     images = os.listdir(app.config['UPLOAD_FOLDER'])
-#     return render_template('index.html', images=images)
-
-# @app.route('/upload', methods=['POST'])
-# def upload():
-#     if 'photo' in request.files:
-#         file = request.files['photo']
-#         if file.filename != '':
-#             file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
-#     return redirect(url_for('gallery'))
-
 import os
 from flask import Flask, render_template, request, redirect, url_for
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'static/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-@app.route('/')
-def index():
-    # Список папок (альбомов) в uploads
-    albums = [name for name in os.listdir(UPLOAD_FOLDER)
-              if os.path.isdir(os.path.join(UPLOAD_FOLDER, name))]
-    return render_template('index.html', albums=albums)
-
-@app.route('/album/<album_name>')
-def album(album_name):
-    album_path = os.path.join(UPLOAD_FOLDER, album_name)
-    if not os.path.exists(album_path):
-        return "Альбом не найден", 404
-    photos = os.listdir(album_path)
-    photos = [photo for photo in photos if photo.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))]
-    return render_template('album.html', album=album_name, photos=photos)
-
 @app.route('/upload', methods=['GET', 'POST'])
 def upload():
     albums = [name for name in os.listdir(UPLOAD_FOLDER)
               if os.path.isdir(os.path.join(UPLOAD_FOLDER, name))]
     if request.method == 'POST':
         album = request.form.get('album')
-        file = request.files.get('photo')
-        if album and file and file.filename != '':
-            album_path = os.path.join(UPLOAD_FOLDER, album)
-            if not os.path.exists(album_path):
-                os.makedirs(album_path)
-            file.save(os.path.join(album_path, file.filename))
-            return redirect(url_for('album', album_name=album))
-    return render_template('upload.html', albums=albums)
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    albums = [name for name in os.listdir(UPLOAD_FOLDER)
-              if os.path.isdir(os.path.join(UPLOAD_FOLDER, name))]
-    if request.method == 'POST':
-        # Получаем имя выбранного альбома и нового альбома
-        album = request.form.get('album')
-        new_album = request.form.get('new_album').strip()
+        new_album = request.form.get('new_album', '').strip()
 
         # Если введено имя нового альбома, используем его
         if new_album:
@@ -72,7 +22,8 @@ def upload():
         if album and file and file.filename != '':
             album_path = os.path.join(UPLOAD_FOLDER, album)
             if not os.path.exists(album_path):
-                os.makedirs(album_path)  # Создаем новую папку-альбом
-            file.save(os.path.join(album_path, file.filename))
+                os.makedirs(album_path)
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(album_path, filename))
             return redirect(url_for('album', album_name=album))
     return render_template('upload.html', albums=albums)
